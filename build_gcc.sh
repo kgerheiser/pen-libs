de4a3bab826e57f595d96842e0e8f17417bdbdef
kgerheiser <kgerheiser@icloud.com>
2021-02-26 15:03:47 -0500

    Build gcc

--- build_gcc.sh
+++ build_gcc.sh
@@ -5,9 +5,11 @@ cd $DOWNLOAD_PATH
 wget https://bigsearcher.com/mirrors/gcc/releases/gcc-10.2.0/gcc-10.2.0.tar.gz
 tar -xf gcc-10.2.0.tar.gz
 cd gcc-10.2.0
+./contrib/download_prerequisites
 
 [[ -d build ]] && rm -rf build
 mkdir -p build && cd build
+
 
 ../configure --prefix=${INSTALL_PREFIX} \
              --enable-languages=c,c++,fortran
