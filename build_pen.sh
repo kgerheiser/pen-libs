c57113de879c7b47eea310fa170d47dd1f421b3a
kgerheiser <kgerheiser@icloud.com>
2021-02-26 14:30:43 -0500

    Update

--- build_pen.sh
+++ build_pen.sh
@@ -97,9 +97,9 @@ mkdir -p ${INSTALL_PREFIX}
 
 #./build_fftw3.sh
 #./build_gsl.sh
-./build_gmp.sh
-./build_nlopt.sh
-./build_nhc.sh
+#./build_gmp.sh
+#./build_nlopt.sh
+#./build_nhc.sh
 ./build_chapel.sh
 ./build_ucx.sh
 ./build_pmix.sh
