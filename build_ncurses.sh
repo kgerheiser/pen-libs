4d1e7fb5a04548bacf61b60a0123526b3599e2b6
kgerheiser <kgerheiser@icloud.com>
2021-02-26 12:38:33 -0500

    Build ncurses shared lib (for real)

--- build_ncurses.sh
+++ build_ncurses.sh
@@ -7,6 +7,7 @@ wget https://invisible-mirror.net/archives/ncurses/ncurses-6.2.tar.gz
 tar -xf ncurses-6.2.tar.gz
 cd ncurses-6.2
 
-./configure --prefix=${INSTALL_PREFIX} --with-shared
+./configure --prefix=${INSTALL_PREFIX} --with-shared --without-normal --without-debug
+
 make -j${NUM_BUILD_THREADS}
 make install
