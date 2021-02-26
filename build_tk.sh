e31a638322def50c9cd33e295a6b457b0897db16
kgerheiser <kgerheiser@icloud.com>
2021-02-26 12:54:44 -0500

    cd to unix directory when building tk

--- build_tk.sh
+++ build_tk.sh
@@ -5,7 +5,7 @@ cd $DOWNLOAD_PATH
 
 wget https://pilotfiber.dl.sourceforge.net/project/tcl/Tcl/8.6.11/tcl8.6.11-src.tar.gz
 tar -xf tcl8.6.11-src.tar.gz
-cd tcl8.6.11
+cd tcl8.6.11/unix
 
 
 ./configure --prefix=${INSTALL_PREFIX}
@@ -15,7 +15,7 @@ make install
 cd $DOWNLOAD_PATH
 wget https://versaweb.dl.sourceforge.net/project/tcl/Tcl/8.6.11/tk8.6.11.1-src.tar.gz
 tar -xf tk8.6.11-src.tar.gz
-cd tk8.6.11
+cd tk8.6.11/unix
 
 ./configure --prefix=${INSTALL_PREFIX} --with-tcl=${INSTALL_PREFIX}
 make -j${NUM_BUILD_THREADS}
