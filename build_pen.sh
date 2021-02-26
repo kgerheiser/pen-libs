ebdd2a2e11aeda8aac880b35e9ed02d286e77e82
kgerheiser <kgerheiser@icloud.com>
2021-02-26 14:19:39 -0500

    Update

--- build_pen.sh
+++ build_pen.sh
@@ -90,8 +90,8 @@ mkdir -p ${INSTALL_PREFIX}
 # #Needs to be setup with xz flags, and probably more
 # #./build_r.sh
 
-./build_pcre.sh
-./build_swig.sh
+#./build_pcre.sh
+#./build_swig.sh
 #./build_valgrind.sh
 ./build_gnuplot.sh
 
