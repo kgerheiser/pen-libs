f951d5343d200c1fe2dbc77bd49573670ddee046
kgerheiser <kgerheiser@icloud.com>
2021-02-26 11:43:06 -0500

    Build bzip2 shared lib

--- build_bzip2.sh
+++ build_bzip2.sh
@@ -7,8 +7,5 @@ wget https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz
 tar -xf bzip2-1.0.8.tar.gz
 cd bzip2-1.0.8
 
-# bzip2 builds a static lib and can cause problems when linking to shared libraries
-# so add -fPIC
-export CFLAGS="-fPIC"
-make
+make -f Makefile-libbz2_so
 make install PREFIX=${INSTALL_PREFIX}
