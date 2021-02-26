c28258f0edb312a21e209c8baf49c86c8e096d87
kgerheiser <kgerheiser@icloud.com>
2021-02-26 12:30:16 -0500

    Include curses for libedit

--- build_libedit.sh
+++ build_libedit.sh
@@ -7,6 +7,8 @@ wget https://thrysoee.dk/editline/libedit-20210216-3.1.tar.gz
 tar -xf libedit-20210216-3.1.tar.gz
 cd libedit-20210216-3.1
 
+export CFLAGS="-I${INSTALL_PREFIX}/include -I${INSTALL_PREFIX}/include/ncurses"
+
 ./configure --prefix=${INSTALL_PREFIX}
 make -j${NUM_BUILD_THREADS}
 make install
