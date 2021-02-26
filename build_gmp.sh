2e493d38392f75340b2746047fc504aec4584c1b
kgerheiser <kgerheiser@icloud.com>
2021-02-26 14:22:59 -0500

    Extract correct file

--- build_gmp.sh
+++ build_gmp.sh
@@ -4,7 +4,7 @@ set -eux
 cd $DOWNLOAD_PATH
 
 wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz
-tar -xf gmp-6.2.1.tar.lz
+tar -xf gmp-6.2.1.tar.xz
 cd gmp-6.2.1
 
 ./configure --prefix=${INSTALL_PREFIX}
