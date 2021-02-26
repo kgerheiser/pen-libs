65e2c1b8b03cc8bdf4a005daa196bd195e3fe94b
kgerheiser <kgerheiser@icloud.com>
2021-02-26 14:09:16 -0500

    add pcre option to swig

--- build_swig.sh
+++ build_swig.sh
@@ -7,6 +7,6 @@ wget https://pilotfiber.dl.sourceforge.net/project/swig/swig/swig-4.0.2/swig-4.0
 tar -xf swig-4.0.2.tar.gz
 cd swig-4.0.2
 
-./configure --prefix=${INSTALL_PREFIX}
+./configure --prefix=${INSTALL_PREFIX} --with-pcre-prefix=${INSTALL_PREFIX}
 make -j${NUM_BUILD_THREADS}
 make install
