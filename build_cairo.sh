41255b2184bccfb9c7c2e9263ade828063648260
kgerheiser <kgerheiser@icloud.com>
2021-02-26 11:20:25 -0500

    Build freetype2

--- build_cairo.sh
+++ build_cairo.sh
@@ -3,6 +3,17 @@ set -eux
 
 cd $DOWNLOAD_PATH
 
+# freetype2 is required by Cairo
+
+wget https://download.savannah.gnu.org/releases/freetype/freetype-2.10.3.tar.gz
+tar -xf freetype-2.10.3.tar.gz
+cd freetype-2.10.3
+./configure --prefix=${INSTALL_PREFIX}
+make -j${NUM_BUILD_THREADS}
+make install
+
+cd $DOWNLOAD_PATH
+
 # pixman is required by cairo
 
 wget https://www.cairographics.org/releases/pixman-0.40.0.tar.gz
