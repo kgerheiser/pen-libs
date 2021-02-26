aab953e1364be7e39a3c59d4fd3f9879f82f6741
kgerheiser <kgerheiser@icloud.com>
2021-02-26 14:11:52 -0500

    Build pcre(1) not pcre v2

--- build_pcre.sh
+++ build_pcre.sh
@@ -3,9 +3,9 @@ set -eux
 
 cd $DOWNLOAD_PATH
 
-wget https://ftp.pcre.org/pub/pcre/pcre2-10.36.tar.gz
-tar -xf pcre2-10.36.tar.gz
-cd pcre2-10.36
+wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz
+tar -xf pcre-8.44.tar.gz
+cd pcre-8.44
 
 ./configure --prefix=${INSTALL_PREFIX}
 make -j${NUM_BUILD_THREADS}
