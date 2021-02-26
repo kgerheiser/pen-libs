24f606a1e6a1aa7bd15fb1d92d1aef786b04581c
kgerheiser <kgerheiser@icloud.com>
2021-02-26 14:24:46 -0500

    Change gmp download

--- build_gmp.sh
+++ build_gmp.sh
@@ -3,8 +3,8 @@ set -eux
 
 cd $DOWNLOAD_PATH
 
-wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz
-tar -xf gmp-6.2.1.tar.xz
+wget https://gmplib.org/download/gmp/gmp-6.2.1.tar.zst
+tar -xf gmp-6.2.1.tar.zst
 cd gmp-6.2.1
 
 ./configure --prefix=${INSTALL_PREFIX}
