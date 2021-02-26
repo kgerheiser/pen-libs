d7f6eadce1b2b3d167ebaed4927faebf9dc6cfcf
kgerheiser <kgerheiser@icloud.com>
2021-02-26 13:03:03 -0500

    Fix tcl directory option

--- build_tk.sh
+++ build_tk.sh
@@ -17,7 +17,7 @@ wget https://versaweb.dl.sourceforge.net/project/tcl/Tcl/8.6.11/tk8.6.11.1-src.t
 tar -xf tk8.6.11.1-src.tar.gz
 cd tk8.6.11/unix
 
-./configure --prefix=${INSTALL_PREFIX} --with-tcl=${INSTALL_PREFIX}
+./configure --prefix=${INSTALL_PREFIX} --with-tcl=${INSTALL_PREFIX}/lib
 make -j${NUM_BUILD_THREADS}
 make install
 
