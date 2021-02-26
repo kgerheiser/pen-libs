880b9e920ba05e5d90fdeacab4df5c9968fbdfbc
kgerheiser <kgerheiser@icloud.com>
2021-02-26 14:59:37 -0500

    Updat mpc version

--- build_mpc.sh
+++ build_mpc.sh
@@ -3,9 +3,9 @@ set -eux
 
 cd $DOWNLOAD_PATH
 
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-0.8.1.tar.gz
-tar zxvf mpc-0.8.1.tar.gz
-cd mpc-0.8.1
+wget https://gcc.gnu.org/pub/gcc/infrastructure/mpc-1.0.3.tar.gz
+tar zxvf mpc-1.0.3.tar.gz
+cd mpc-1.0.3
 
 ./configure --prefix=${INSTALL_PREFIX} \
             --with-gmp=${INSTALL_PREFIX} --with-mpfr=${INSTALL_PREFIX}
